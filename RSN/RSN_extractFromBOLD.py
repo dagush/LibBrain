0bf51d51074942ad327413fdbf4e340fbab66e29
dagush <gustavo.patow@udg.edu>
2025-10-19 19:27:56 +0200

    fixed and updated old RSN code into neuronumba and modern LibBrain...

--- RSN/RSN_extractFromBOLD.py
+++ RSN/RSN_extractFromBOLD.py
@@ -1,24 +1,33 @@
 # -------------------------------------------------------------------------
-#  Extract RSN signals from a whole-brain fMRI signal
+#  Extract RSN-seppartaed BOLD signals from a general BOLD file
+#
 #  by Gustavo Patow
 # -------------------------------------------------------------------------
-import csv
+import numpy as np
+import scipy.io as sio
+
+import RSN.RSN_transfer as transfer
 
 
-def readIndicesFile(filePath):
-    res = {}
-    with open(filePath, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            res |= {row['RSN Label']: row['Indices']}
-    return res
+# -------------------------------------------------------------------------
+# Convenience method to save results in MATLAB format
+def saveRSN_Matlab(rsnBOLDs, fileName, saveSufix=''):
+    data = {}
+    for subject in rsnBOLDs:
+        networks = rsnBOLDs[subject]
+        proc_rsn = np.array([(n, networks[n]) for n in networks], dtype=object)
+        data[saveSufix+subject] = proc_rsn
+    sio.savemat(fileName, data)
 
 
+# -------------------------------------------------------------------------
+# RSN grouping code
+# -------------------------------------------------------------------------
 def simplifyIndicesFile(rsnIndices, subset):
     return {k: rsnIndices[k] for k in subset}
 
 
-def extractSubjectRSNfMRI(BOLD, namesAndIDs):
+def extract_subjectfMRI_RSN(BOLD, namesAndIDs):
     allNames = list(set(namesAndIDs))  # no repeated entries
     res = {}
     for name in allNames:
@@ -29,12 +38,74 @@ def extractSubjectRSNfMRI(BOLD, namesAndIDs):
     return res
 
 
-def extractRSNGroupfMRI(BOLDs, rsn):
+def extract_GroupfMRI_RSN(BOLDs, rsn):
     res = {}
     for s in BOLDs:
-        res[s] = extractSubjectRSNfMRI(BOLDs[s], rsn)
+        res[s] = extract_subjectfMRI_RSN(BOLDs[s], rsn)
     return res
 
+
+def process_group_RSN(data_loader, group, rsn, useLR,
+                      save_result, sufix, RSN_save_folder):
+    # -------- get all fMRIs for a given group
+    all_fMRI = data_loader.get_fullGroup_data(group)
+    fMRIs = {s: all_fMRI[s]['timeseries'] for s in all_fMRI}
+    RSNBOLDs = extract_GroupfMRI_RSN(fMRIs, rsn)
+    if save_result:
+        fileName = RSN_save_folder + f'/RSN-{"14" if useLR else "7"}_{group}{sufix}.mat'
+        saveRSN_Matlab(RSNBOLDs, fileName, saveSufix=sufix)
+        print(f'\nSaved to: {fileName}\n')
+    return RSNBOLDs
+
+
+# -------------------------------------------------------------------------
+#  Takes a DataLoader and some optional parameters, and group all regional
+#  BOLD signals for all the subjects in the groups into their RSNs.
+# -------------------------------------------------------------------------
+def fromBOLD(data_loader,
+             useLR=False,  # True: differentiate between Left and Right hemispheres. False, don't
+             save_result=False,  # True: save results to a MATLAB file
+             fileSufix='',  # saving sufix (usually, something like 'subj_'
+             RSN_save_folder=''  # if we save, where to?
+             ):
+    # ------ first, make sure the basic RSN information is built for the chosen target parcellation
+    target_parc = data_loader.get_parcellation()
+    transfer.build_RSN_for_parcellation(target_parc, plotNodes=False)
+
+    # ------- For the full RSN test
+    print('-------------- Processing full RSN set')
+    rsn = transfer.read_RSN_data(target_parc)
+    # --------------------------------------------------
+    # Process fMRI for all subjects, in all cohorts
+    # --------------------------------------------------
+    res = {}
+    for group in data_loader.get_groupLabels():
+        res[group] = process_group_RSN(data_loader, group, rsn, useLR=useLR,
+                                       save_result=save_result, sufix=fileSufix, RSN_save_folder=RSN_save_folder)
+
+    # ------- For the detailed Default Mode Network test
+    # do_detailed_DMN_test = False
+    # if do_detailed_DMN_test:
+    #     print(f'-------------- Processing detail network for {[k for k in detailNetworks.keys()]}')
+    #     # ------------ For the RSN analysis, but keeping only the detail on the Default Mode network
+    #     rsn = extractRSNFromBOLD.readIndicesFile(indicesFileParcellationRSNDetail)
+    #     rsn = extractRSNFromBOLD.simplifyIndicesFile(rsn, detailNetworksFullNames)  # if want to process ONLY the detail info.
+    #     fileSufix = '-Detail-Default'
+    #     for group in DL.get_groupLabels():
+    #         process_group(group, rsn, useLR=useLR, sufix=fileSufix)
+
+    return res
+
+
+# --------------------------------------------------
+# ------------------- Debug code -------------------
+# --------------------------------------------------
+if __name__ == '__main__':
+    import DataLoaders.ADNI_A as ADNI_A
+    # Groups: HC, MCI, AD
+    # Glasser360 -> DorsAttn: 47, SalVentAttn: 47, Cont: 46, Limbic: 23, Default: 85, SomMot: 55, Vis: 57
+    DL = ADNI_A.ADNI_A()
+    res = fromBOLD(DL)
 
 # -------------------------------------------------------------------------
 # -------------------------------------------------------------------------
--- RSN/RSN_transfer.py
+++ RSN/RSN_transfer.py
@@ -1,34 +1,27 @@
 # ======================================================
 # Resting State Networks transfer between parcellations
+# ----------------------
+# This code uses the Schaefer 1000 Centers of Gravity
+# coords (CoG) to compute the RSN labels for any other
+# parcellation that has, at least, its CoG defined.
 #
 # Code by Gustavo Patow
 # ======================================================
 import csv
 import numpy as np
 from scipy import spatial
-import matplotlib.pyplot as plt
+
+import DataLoaders.WorkBrainFolder as WBF
+from Plotting.plot3DPointCloud import plot_point_cloud
 
 
-def plotParcellation(parcellationData, title=''):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    for region in parcellationData:
-        r = region[2]
-        ax.scatter(r[0], r[1], r[2], marker='o')
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-    ax.set_title(title)
-    plt.show()
-
-
-def readDestinationParcellation(filePath):
-    res = []
-    with open(filePath) as f:
-        lines = f.readlines()
-        for line in lines:
-            res.append(('', '', np.fromstring(line, dtype=float, sep=' ')))
-    return res
+# def readDestinationParcellation(filePath):
+#     res = []
+#     with open(filePath) as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             res.append(('', '', np.fromstring(line, dtype=float, sep=' ')))
+#     return res
 
 
 # Taken from
@@ -44,13 +37,12 @@ def findClosestPoints(reference, target):
     return indexes
 
 
-def assignRSNLabels(referenceSet, targetSet):
+def assignRSNLabels(referenceSet, target_coords):
     # Shoe-horn existing data for entry into KDTree routines
-    referenceArray = np.array([r[2] for r in referenceSet])
-    targetArray = [p[2] for p in targetSet]
-    res = findClosestPoints(referenceArray, targetArray)
+    reference_coords = np.array([r[2] for r in referenceSet])
+    res = findClosestPoints(reference_coords, target_coords)
 
-    targetSetLabelled = [('', referenceSet[res[pos]][1], r[2]) for pos, r in enumerate(targetSet)]
+    targetSetLabelled = [('', referenceSet[res[pos]][1], r) for pos, r in enumerate(target_coords)]
 
     return targetSetLabelled
 
@@ -91,25 +83,9 @@ def indices4RSNs(parcellation):
     return res
 
 
-def parcellationFormat(labelledTarget):
-    parc = [[pos+1, roi[1], roi[2][0], roi[2][1], roi[2][2]] for pos,roi in enumerate(labelledTarget)]
-    return parc
-
-
 # ================================================================
 # Load and save parcellation data
 # ================================================================
-def saveParcellation2CSV(filename, parcellation):
-    header = ["ROI Label", "ROI Name", "R", "A", "S"]
-    with open(filename, 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        # write the header
-        writer.writerow(header)
-        # write the data
-        for r in parcellation:
-            writer.writerow(r)
-
-
 def readReferenceRSN(filePath, roundCoords=True):
     res = []
     with open(filePath, newline='') as csvfile:
@@ -122,16 +98,55 @@ def readReferenceRSN(filePath, roundCoords=True):
     return res
 
 
-def saveRSNIndices(idxs, outFile):
+def read_RSN_data(target_parcellation, useLR=False):
+    parc_name = target_parcellation.get_name() + '-' + str(target_parcellation.get_N())
+    file_RSN = WBF.WorkBrainProducedDataFolder + '_Parcellations/' + parc_name + '_RSN.csv'
+    file_indices = WBF.WorkBrainProducedDataFolder + '_Parcellations/' + parc_name + f'_RSN_{"14" if useLR else "7"}_indices.csv'  # if we do NOT use detailed regions
+    file_labels = WBF.WorkBrainProducedDataFolder + '_Parcellations/' + parc_name + f'_RSN_{"14" if useLR else "7"}_RSN_labels.csv'  # if we do NOT use detailed regions
+
+    res_indices = {}
+    with open(file_indices, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            res_indices |= {row['RSN Label']: row['Indices']}
+
+    return res_indices
+
+
+def save_parcellation_info(target_parcellation, parcellation_info):
+    # this keeps all the original names, no distinctions whether we use detailed regions or not...
+    parc_name = target_parcellation.get_name() + '-' + str(target_parcellation.get_N())
+    filename = WBF.WorkBrainProducedDataFolder + '_Parcellations/' + \
+               parc_name + '_RSN.csv'
+    header = ["ROI Label", "ROI Name", "R", "A", "S"]
+    with open(filename, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        # write the header
+        writer.writerow(header)
+        # write the data
+        for r in parcellation_info:
+            writer.writerow(r)
+    print(f'saved parcellation to: {filename}')
+
+
+def save_RSN_Indices(target_parcellation, idxs, useLR=False):
+    # The outfile should change according to whether we use or not detailed regions...
+    parc_name = target_parcellation.get_name() + '-' + str(target_parcellation.get_N())
+    outFile = WBF.WorkBrainProducedDataFolder + '_Parcellations/' + \
+              parc_name + f'_RSN_{"14" if useLR else "7"}_indices.csv'  # if we do NOT use detailed regions
     header = ["RSN Label", "Indices"]
     with open(outFile, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         for rsn in idxs:
             writer.writerow([rsn, idxs[rsn]])
+    print(f'saved indices to: {outFile}')
 
 
-def saveRSN_labels(idxs, outFile):
+def save_RSN_labels(target_parcellation, idxs, useLR=False):
+    parc_name = target_parcellation.get_name() + '-' + str(target_parcellation.get_N())
+    outFile = WBF.WorkBrainProducedDataFolder + '_Parcellations/' + \
+              parc_name + f'_RSN_{"14" if useLR else "7"}_RSN_labels.csv'  # if we do NOT use detailed regions
     # header = ["RSN Label"]
     with open(outFile, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
@@ -140,59 +155,61 @@ def saveRSN_labels(idxs, outFile):
             writer.writerow([rsn])
 
 
-def RSN_Glasser360():
-    import DataLoaders.WorkBrainFolder as WBF
+# -----------------------------------------------------------------
+# Convenience method to transfer from a parcellation
+# -----------------------------------------------------------------
+def build_RSN_for_parcellation(
+        target_parcellation,
+        plotNodes=False,
+        useLR=False,
+        detailNetworks={},  # If a mode detailed region is NOT needed, use an empty detailNetworks
+                            # detailNetworks = {'Default': ['PFC', 'Par', 'Temp', 'pCunPCC', 'PHC']}
+                            # If a more detailed region is needed, especify it here (see comment for collectNamesAndIDsRSN)
+        ):
     numNodes = 1000
     # -------- As input, we are going to use Yeo's 1000 roi RSN info on Schaefer's 2018 parcellation
     inPath = WBF.WorkBrainDataFolder + '_Parcellations/'
     inFileNameRef = f'Schaefer2018/MNI/Centroid_coordinates/Schaefer2018_{numNodes}Parcels_7Networks_order_FSLMNI152_1mm.Centroid_RAS.csv'
-    inFileNameTarget = 'Glasser360/Glasser360_coords.txt'
+    # inFileNameTarget = 'Glasser360/Glasser360_coords.txt'
 
-
-    detailNetworks = {}  # If a mode detailed region is NOT needed, use an empty detailNetworks
-    # detailNetworks = {'Default': ['PFC', 'Par', 'Temp', 'pCunPCC', 'PHC']}  # If a more detailed region is needed, especify it here (see comment for collectNamesAndIDsRSN)
-
-    plotNodes = False
 
     # Read all reference values, i.e., from Yeo's Schaefer2018 RSN labels
     dataRef = readReferenceRSN(inPath+inFileNameRef)
     print(f'we have {len(dataRef)} elements')
     if plotNodes:
-        plotParcellation(dataRef, title='Ref')
+        coords_ref = [reg[2] for reg in dataRef]
+        plot_point_cloud(coords_ref, title='Ref')
 
     # read all coords for the target parcellation (here, Glasser360)
-    dataTarget = readDestinationParcellation(inPath+inFileNameTarget)
-    print(f'we have {len(dataTarget)} elements')
+    # dataTarget = readDestinationParcellation(inPath+inFileNameTarget)
+    coords_target = target_parcellation.get_CoGs()
+    print(f'we have {len(coords_target)} elements')
     if plotNodes:
-        plotParcellation(dataTarget, title='Target')
+        plot_point_cloud(coords_target, title='Target')
 
     # First, transfer the closest RSN label to each node of the target parcellation
-    labelledTarget = assignRSNLabels(dataRef, dataTarget)
-    # this keeps all the originalnames, no distinctions whether we use detailed regions or not...
-    outPath = WBF.WorkBrainProducedDataFolder + '_Parcellations/Glasser360RSN.csv'
-    saveParcellation = parcellationFormat(labelledTarget)
-    saveParcellation2CSV(outPath, saveParcellation)
-    print(f'saved parcellation to: {outPath}')
-
-    # This groups RSN labels into fewer sets, grouping by RSN name and, if needed, subregion name
-    useLR = False
-    names = collectNamesRSN(saveParcellation, useLR=useLR, detailedRSNs=detailNetworks)
-    RSN_names = list(set(names))
-    outPath = WBF.WorkBrainProducedDataFolder + f'/_Parcellations/Glasser360RSN_{"14" if useLR else "7"}_RSN_labels.csv'  # if we do NOT use detailed regions
-    saveRSN_labels(names, outPath)
+    labelledTarget = assignRSNLabels(dataRef, coords_target)
+    # Second, extract the parcellation info in a formatted list
+    formatted_parc = [[pos+1, roi[1], roi[2][0], roi[2][1], roi[2][2]] for pos, roi in enumerate(labelledTarget)]
+    # Third, group RSN labels into fewer sets, grouping by RSN name and, if needed, subregion name
+    names = collectNamesRSN(formatted_parc, useLR=useLR, detailedRSNs=detailNetworks)
     print(f'Names collected: {list(set(names))}')
+    # Fourth, generate the indices
     i = indices4RSNs(names)
-    # The outfile should change according to whether we use or not detailed regions...
-    outPath = WBF.WorkBrainProducedDataFolder + f'/_Parcellations/Glasser360RSN_{"14" if useLR else "7"}_indices.csv'  # if we do NOT use detailed regions
-    saveRSNIndices(i, outPath)
-    print(f'saved indices to: {outPath}')
+
+    # ------- save!!!
+    save_parcellation_info(target_parcellation, formatted_parc)
+    save_RSN_labels(target_parcellation, names)
+    save_RSN_Indices(target_parcellation, i)
 
 
 # ==================================================================
 # test code: transfer RSNs to the Glasser360 parcellation
 # ==================================================================
 if __name__ == '__main__':
-    RSN_Glasser360()
+    import DataLoaders.Parcellations.Glasser379 as Glasser379
+    parc = Glasser379.Glasser379(N=360)
+    build_RSN_for_parcellation(parc, plotNodes=False)
 
 # ======================================================
 # ======================================================
